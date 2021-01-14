e5f569e8ef48f979859772a573690dfa0ef668ce
Sandbox 🤖 <neo4j-oss-build@neotechnology.com>
2021-01-14 15:38:37 +0000

    Updating sandbox recommendations

--- code/python/example.py
+++ code/python/example.py
@@ -4,7 +4,7 @@
 from neo4j import GraphDatabase, basic_auth
 
 driver = GraphDatabase.driver(
-  "bolt://<HOST>:<BOLTPORT>", 
+  "bolt://<HOST>:<BOLTPORT>",
   auth=basic_auth("<USERNAME>", "<PASSWORD>"))
 
 cypher_query = '''
@@ -15,8 +15,7 @@ RETURN distinct rec.title AS recommendation LIMIT 20
 with driver.session(database="neo4j") as session:
   results = session.read_transaction(
     lambda tx: tx.run(cypher_query,
-      movie="Crimson Tide").data())
-
+                      movie="Crimson Tide").data())
   for record in results:
     print(record['recommendation'])
 
