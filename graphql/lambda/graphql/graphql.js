2b2d51158d0be1f95f2d1c3d633b76bcbdaf0321
johnymontana <lyonwj@gmail.com>
2022-06-05 19:15:41 -0500

    update to latest neo4j graphql library version

--- graphql/lambda/graphql/graphql.js
+++ graphql/lambda/graphql/graphql.js
@@ -1,4 +1,7 @@
 const { ApolloServer } = require("apollo-server-lambda");
+const {
+  ApolloServerPluginLandingPageGraphQLPlayground,
+} = require("apollo-server-core");
 const neo4j = require("neo4j-driver");
 const { Neo4jGraphQL } = require("@neo4j/graphql");
 const fs = require("fs");
@@ -9,30 +12,39 @@ const typeDefs = fs
   .readFileSync(require.resolve("./schema.graphql"))
   .toString("utf-8");
 
-const neoSchema = new Neo4jGraphQL({
-  typeDefs,
-});
-
 const driver = neo4j.driver(
   process.env.NEO4J_URI,
   neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD)
 );
 
-const server = new ApolloServer({
-  schema: neoSchema.schema,
-  context: ({ event }) => {
-    return {
-      driver,
-      driverConfig: { database: process.env.NEO4J_DATABASE },
-    };
-  },
-  introspection: true,
-  playground: true,
+const neoSchema = new Neo4jGraphQL({
+  typeDefs,
+  driver,
 });
 
-exports.handler = server.createHandler({
-  cors: {
-    origin: "*",
-    credentials: true,
-  },
-});
+const initServer = async () => {
+  return await neoSchema.getSchema().then((schema) => {
+    const server = new ApolloServer({
+      schema,
+      playground: true,
+      introspection: true,
+      plugins: [ApolloServerPluginLandingPageGraphQLPlayground],
+      context: ({ event }) => ({ req: event }),
+    });
+    const serverHandler = server.createHandler();
+    return serverHandler;
+  });
+};
+
+exports.handler = async (event, context, callback) => {
+  const serverHandler = await initServer();
+
+  return serverHandler(
+    {
+      ...event,
+      requestContext: event.requestContext || {},
+    },
+    context,
+    callback
+  );
+};
